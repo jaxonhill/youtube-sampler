7f7a40a94dfb5b53cbb3abcb2eae12269c63f5c2
jaxonhill <jaxoncharleshill@gmail.com>
2022-12-04 00:11:37 -0800

    Refactor post request to get audio link, switch to JSON instead of html page

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from pytube import YouTube
 import re
 
@@ -8,34 +8,36 @@ YOUTUBE_REGEX = re.compile(
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.post("/youtube")
+def youtube_link():
+    # Get the link from the post request
+    link = request.values.get("ytlink")
+
+    try:
+        # Create the YouTube object and get the title
+        yt = YouTube(link)
+        title = yt.title
+
+        # Filter the streams to get only get the highest quality audio stream
+        audio_stream = yt.streams.get_audio_only()
+
+        # Get the URL of the audio stream
+        audio_url = audio_stream.url
+
+        # Return JSON to user on client side
+        return jsonify(
+            {
+                "link": link,
+                "title": title,
+                "audio_url": audio_url,
+            }
+        )
+    except:
+        return jsonify({"error": "Failed to fetch the YouTube information."})
+
+
+@app.get("/")
 def index():
-    if request.method == "POST":
-        # Get the link from the post request
-        link = request.form.get("ytlink")
-
-        try:
-            # Create the YouTube object and get the title
-            yt = YouTube(link)
-            title = yt.title
-
-            # Filter the streams to get only get the highest quality audio stream
-            audio_stream = yt.streams.get_audio_only()
-
-            # Get the URL of the audio stream
-            audio_url = audio_stream.url
-
-            # Send the information to the user on the client side
-            return render_template(
-                "success.html",
-                link=link,
-                title=title,
-                stream=audio_stream,
-                audio_url=audio_url,
-            )
-        except:
-            return render_template("failure.html")
-
     return render_template("index.html")
 
 
