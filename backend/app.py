feade4a6cc123cb050b2e04af57779902462fadc
jaxonhill <jaxoncharleshill@gmail.com>
2022-12-04 02:02:34 -0800

    Add cors cross origin and make POST request from React

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 from pytube import YouTube
 import re
+import json
 
 YOUTUBE_REGEX = re.compile(
     r"^(https?:\/\/)?(www\.)?youtube\.com\/(watch\?v=)?[a-zA-Z0-9_-]+$"
 )
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
+@cross_origin()
 @app.post("/youtube")
 def youtube_link():
     # Get the link from the post request
-    link = request.values.get("ytlink")
+    print(request.method)
+    json_data = request.get_json()
 
     try:
+        link = json_data["ytlink"]
         # Create the YouTube object and get the title
         yt = YouTube(link)
         title = yt.title
@@ -25,15 +32,25 @@ def youtube_link():
         audio_url = audio_stream.url
 
         # Return JSON to user on client side
-        return jsonify(
+        response = jsonify(
             {
                 "link": link,
                 "title": title,
                 "audio_url": audio_url,
             }
         )
+
+        return response
+
     except:
-        return jsonify({"error": "Failed to fetch the YouTube information."})
+        response = jsonify(
+            {
+                "error": "Failed to fetch the YouTube information.",
+            }
+        )
+
+        return response
+        # return jsonify({"error": "Failed to fetch the YouTube information."})
 
 
 @app.get("/")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [content, setContent] = useState({});
+
+  const fetchURLInfo = (e) => {
+    e.preventDefault();
+    const url = "http://localhost:5000/youtube";
+
+    // Options for the fetch function to make it a POST
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ytlink: inputText }),
+    };
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => setContent(data))
+  }
+
+  console.log(content);
+  console.log(inputText);
+
   return (
-    <div>Hello World!</div>
+    <div>
+      <form>
+        <input
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+          type="text" name="ytlink" required placeholder="YouTube URL"
+        />
+        <button onClick={(e) => fetchURLInfo(e)}>Download</button>
+      </form>
+      {content ? <p>{content["title"]}</p> : <p></p>}
+    </div>
   );
 }
 
